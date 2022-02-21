cbf01b770c90bd96ff9c6bab9259840477ef5a98
Tara Culpin <taraculpin@gmail.com>
2022-02-21 17:59:12 +0000

    Added routes for pets, bookings, my_pets, my_bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pets#home'
+  resources :pets do
+    resources :bookings, except: :destroy
+  end
+  resources :bookings, only: :destroy
+  get 'my_pets', to: 'pets#my_pets'
+  get 'my_bookings', to: 'bookings#my_bookings'
 end
