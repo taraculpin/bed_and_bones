ad80ab42f2eaa03bad1faa4ec83a058c04199f04
Solomon Karim <93667920+Solkarim91@users.noreply.github.com>
2022-02-24 14:24:20 +0000

    finished working with geocode and added autocomplete to the address field on the new pet form

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -7,15 +7,6 @@ export default class extends Controller {
     markers: Array
   }
 
-  // connect() {
-  //   mapboxgl.accessToken = this.apiKeyValue
-
-  //   this.map = new mapboxgl.Map({
-  //     container: this.element,
-  //     style: "mapbox://styles/mapbox/streets-v10"
-  //   })
-  // }
-
   connect() {
     // [...]
     mapboxgl.accessToken = this.apiKeyValue
