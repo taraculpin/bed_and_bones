9ab48f4ffcc62bb93cc464283d1c4c257c39e2d1
Jeremiah Harriott <harriott_47@hotmail.com>
2022-02-24 13:21:50 +0000

    created new and create in controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+
+    def new
+      @pets = Pet.find(params[:pets_id])
+      @review = Pet.new
+    end
+
+    def create
+      @review = Review.new(review_params)
+      @pets = Pet.find(params[:pets_id])
+      @review.pets = @pets
+
+      if @review.save
+        redirect_to pet_path(@pets)
+      else
+        render :new
+      end
+    end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :pets do
-  resources :bookings, only: [:show, :new, :create, :edit, :update] do
+    resources :bookings, only: [:show, :new, :create, :edit, :update] do
       resources :reviews, only: [:new, :create]
     end
     resources :bookings, only: :destroy
     get 'my_pets', to: 'pets#my_pets'
     get 'my_bookings', to: 'bookings#my_bookings'
-
-    resources :reviews, only: :destroy
-end
+  end
 end
