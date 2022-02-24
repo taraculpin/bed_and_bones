c78f3e075aaeebde283e13370863664ad3b85f69
Jeremiah Harriott <harriott_47@hotmail.com>
2022-02-24 12:40:43 +0000

    added reviews resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :pets do
     resources :bookings, only: [:show, :new, :create, :edit, :update]
+    resources :reviews, only: {:new, :create }
   end
   resources :bookings, only: :destroy
   get 'my_pets', to: 'pets#my_pets'
   get 'my_bookings', to: 'bookings#my_bookings'
+
+  resources :reviews, only {:destroy}
 end
