fb62f234214f3c5791105dc2032d577643fce5bd
Jeremiah Harriott <harriott_47@hotmail.com>
2022-02-24 12:31:56 +0000

    Inside of reviews model validations added

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ApplicationRecord
+  class Review < ApplicationRecord
+    belongs_to :Booking
+    validates :content, presence: true
+    validates :rating, inclusion: { in: 0..10 }, numericality: { only_integer: true }
+  end
 end
